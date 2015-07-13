6eb88216d0bfe71df92b11680ea495210092cf7e
ShapeDev <nmartinez@smcpartners.com>
2015-07-13 09:45:44 -0400

    sort tables and admin panels

---
+++ src/main/webapp/js/sorttable.js
@@ -0,0 +1,23 @@
+(function (window) {
+    //'use strict';
+
+    var app = document.querySelector('#app');
+    var appAuth = null;
+
+    app.addEventListener('template-bound', function () {
+        var testDlg = document.querySelector("#testDlg");
+        var testDemIn = document.querySelector("#testDemIn");
+        var loginPage = document.querySelector('#loginPage');
+        //var mainPage = document.querySelector('#mainPage');
+        console.log('Our app is ready to rock!');
+    });
+
+    // See https://github.com/Polymer/polymer/issues/1381
+    window.addEventListener('WebComponentsReady', function () {
+        document.querySelector('body').removeAttribute('unresolved');
+        appAuth = document.querySelector("#appAuth");
+        appAuth.initialize();
+        app.select = "login";
+    });
+
+})(window);
